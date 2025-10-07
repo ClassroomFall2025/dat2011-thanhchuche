d7b2c5fecd4a3a797cc4c21754456470b12f6653
thanhchuche <thuu53083@gmail.com>
2025-10-07 08:25:07 +0700

    đề_

---
+++ asm1.py
@@ -0,0 +1,32 @@
+import json
+import os   
+
+def nhap_nhan_vien():
+    print("Nhập danh sách nhân viên và lưu vào file")
+
+def xuat_danh_sach():
+    print("Đọc từ file và xuất danh sách nhân viên")
+
+def tim_theo_ma():
+    print("Tìm và hiển thị nhân viên theo mã")
+
+def xoa_nhan_vien():
+    print("Xóa nhân viên theo mã và cập nhật file")
+
+def cap_nhap_nhan_vien():
+    print("Cập nhật thông tin nhân viên theo mã và lưu vào file")
+
+def tim_theo_khoan_luong():
+    print("Tìm nhân viên theo khoản lương")
+
+def sap_xep_theo_ten():
+    print("Sắp xếp nhân viên theo họ và tên")
+
+def sap_xep_theo_thu_nhap():
+    print("Sắp xếo nhân viên theo thu nhập")
+
+def top5_thu_nhap():
+    print("Hiển thị 5 nhân viên có thu nhập cao nhất")
+
+def thoat():
+    print("Thoát chương trình")
