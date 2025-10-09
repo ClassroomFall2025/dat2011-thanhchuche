9bdcc30df2aab9b9908fe543224cc2d9b53923d0
Nguyen Huu Thanh <thuu53083@gmail.com>
2025-10-09 22:40:26 +0700

    bai1lab5

--- bai1lab5.py
+++ bai1lab5.py
@@ -1,6 +1,6 @@
-class Sanpham1:
-    def __init__(self, ten, gia, giam_gia):
-        self.ten = ten 
+class SanPham1:
+    def __init__(self, ten_san_pham, gia, giam_gia):
+        self.ten_san_pham = ten_san_pham
         self.gia = gia 
         self.giam_gia = giam_gia
     
@@ -8,7 +8,7 @@ class Sanpham1:
         return self.gia * 0.1    
     
     def xuat_thong_tin(self):
-        print(f"Tên sản phẩm: {self.ten}")
+        print(f"Tên sản phẩm: {self.ten_san_pham}")
         print(f"Đơn giá: {self.gia:,.0f} VND")
         print(f"Giảm giá: {self.giam_gia:,.0f} VND")
         print(f"Thuế nhập khẩu: {self.tinh_thue_nhap_khau():,.0f} VND")
