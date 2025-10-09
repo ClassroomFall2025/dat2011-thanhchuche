5231eb06532892e1fa6cfe64b34968192cd6a5fd
Nguyen Huu Thanh <thuu53083@gmail.com>
2025-10-09 21:49:07 +0700

    bai1lab5

---
+++ bai1lab5.py
@@ -0,0 +1,13 @@
+class Sanpham:
+    def __int__(self, ten, gia, giam_gia):
+        self.ten = ten 
+        self.gia = gia 
+        self.giam_gia = giam_gia
+    def tinh_thue_nhap_khau(self):
+        return self.gia * 0.1    
+    def xuat_thong_tin(self):
+        print(f"Tên sản phẩm: {self.ten}")
+        print(f"Đơn giá: {self.gia:,.0f} VND")
+        print(f"Giảm giá: {self.giam_gia:,.0f} VND")
+        print(f"Thuế nhập khẩu: {self.tinh_thue_nhap_khau:,.0f} VND")
+        print("===========================")
