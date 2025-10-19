5e19631a539b5378d01faa5a48caf24b37a42b7a
Nguyen Huu Thanh <thuu53083@gmail.com>
2025-10-20 01:08:27 +0700

    ASM

--- chucnang.py
+++ chucnang.py
@@ -96,15 +96,19 @@ def xuat_danh_sach():
     for nv in ds:
         print(nv)
 
-def tim_theo_ma(ds):
+def tim_theo_ma():
     ma = input("Nhập mã nhân viên cần tìm: ")
     found = [nv for nv in ds if nv.ma == ma]
     if found:
-        xuat_danh_sach(found)
+        print("\n Kết quả nhân viên cần tìm: ")
+        print("{:<10} | {:<25} | {:>10} | {:>12} | {:>10}".format("Mã", "Họ tên", "Lương", "Thu nhập", "Thuế"))
+        print("-" * 80)
+        for nv in found:
+            print(nv) 
     else: 
         print("không tìm thấy nhân viên")
-
-def xoa_theo_ma(ds):
+        
+def xoa_theo_ma():
     ma = input("Nhập mã nhân viên cần xóa: ")
     for nv in ds:
         if nv.ma == ma:
@@ -113,12 +117,12 @@ def xoa_theo_ma(ds):
             return
     print("Không tìm thấy nhân viên.")
 
-def cap_nhat_thong_tin(ds):
+def cap_nhat_thong_tin():
     ma = input("Nhập mã nhân viên cần cập nhât: ")
     for nv in  ds:
         if nv.ma == ma:
-            nv.ho_ten = input("Nhập họ tên mới: ") or nv.ho_ten
-            luong_moi = input("Nhập lương mới: ")
+            nv.ho_ten = input("Nhập họ tên mới ({nv.ho_ten}): ") or nv.ho_ten
+            luong_moi = input("Nhập lương mới ({nv.luong}): ")
             if luong_moi:
                 nv.luong = float(luong_moi)
             print("đã cập nhật thành công thông tin thành công.")
@@ -126,23 +130,29 @@ def cap_nhat_thong_tin(ds):
     print("Không tìm thấy nhân viên.")
 
 
-def tim_theo_khoang_luong(ds):
+def tim_theo_khoang_luong():
     min_1 = float(input("Nhập lương tối thiểu: "))
     max_1 = float(input("Nhập lương tối đa: "))
     ket_qua = [nv for nv in ds if min_1 <= nv.luong <= max_1]
-    xuat_danh_sach(ket_qua) if ket_qua else print ("không có nhân viên trong khoảng này.")
-
-def sap_xep_theo_ho_te(ds):
+    if ket_qua:
+        print("\n✅ Nhân viên trong khoảng lương:")
+        print("{:<10} | {:<25} | {:>10} | {:>12} | {:>10}".format("Mã", "Họ tên", "Lương", "Thu nhập", "Thuế"))
+        print("-" * 80)
+        for nv in ket_qua:
+            print(nv)
+    else:
+        print("❌ Không có nhân viên trong khoảng này.")
+def sap_xep_theo_ho_ten():
     ds.sort(key=lambda nv: nv.ho_ten)
     print("Đã sắp xếp theo họ tên.")
     xuat_danh_sach(ds)
 
-def sap_xep_theo_thu_nhap(ds):
+def sap_xep_theo_thu_nhap():
     ds.sort(key=lambda nv: nv.tinh_thu_nhap(), reverse = True)
     print("Đã sắp xếp theo thu nhập.")
     xuat_danh_sach(ds)
 
-def top5_thu_nhap(ds):
+def top5_thu_nhap():
     ds_top = sorted(ds, key = lambda nv: nv.tinh_thu_nhap(), reverse = True)[:5]
     print(f"\n top 5 nhân viên có thu nhật cao nhất là: ")
     xuat_danh_sach(ds_top)
