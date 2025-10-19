4a95f7f693d809d476a6e55784b1b2d0550e945b
Nguyen Huu Thanh <thuu53083@gmail.com>
2025-10-20 01:31:06 +0700

    asm

--- chucnangg.py
+++ chucnangg.py
@@ -1,18 +1,19 @@
-def nhap_danh_sach_va_luu_file():
-    print(" Y1. Nhập danh sách nhân viên từ bàn phím. Lưu thông tin nhân viên vào file.")
-def doc_va_xuat_danh_sach():
-    print("Y2. Đọc thông tin nhân viên từ file và xuất danh sách nhân viên ra màn hình.")
-def tim_nhan_vien_theo_ma():
-    print("Y3. Tìm và hiển thị nhân viên theo mã nhập từ bàn phím.")
-def xoa_nhan_vien_theo_ma():
-    print("Y4. Xóa nhân viên theo mã nhập từ bàn phím. Cập nhật file dữ liệu.")
-def cap_nhat_thong_tin_nhan_vien():
-    print("Y5. Cập nhật thông tin nhân viên theo mã nhập từ bàn phím và ghi thay đổi vào file.") 
-def tim_nhan_vien_theo_khoang_luong():
-    print("Y6. Tìm các nhân viên theo khoảng lương nhập từ bàn phím.")
-def sap_xep_theo_ho_ten():
-    print("Y7. Sắp xếp nhân viên theo họ và tên.")
-def sap_xep_theo_thu_nhap():
-    print("Y8. Sắp xếp nhân viên theo thu nhập.")
-def xuat_5_nhan_vien_thu_nhap_cao_nhat():
-    print("Y9. Xuất 5 nhân viên có thu nhập cao nhất.")
+class giaidoan1():
+    def nhap_danh_sach_va_luu_file():
+        print(" Y1. Nhập danh sách nhân viên từ bàn phím. Lưu thông tin nhân viên vào file.")
+    def doc_va_xuat_danh_sach():
+        print("Y2. Đọc thông tin nhân viên từ file và xuất danh sách nhân viên ra màn hình.")
+    def tim_nhan_vien_theo_ma():
+        print("Y3. Tìm và hiển thị nhân viên theo mã nhập từ bàn phím.")
+    def xoa_nhan_vien_theo_ma():
+        print("Y4. Xóa nhân viên theo mã nhập từ bàn phím. Cập nhật file dữ liệu.")
+    def cap_nhat_thong_tin_nhan_vien():
+        print("Y5. Cập nhật thông tin nhân viên theo mã nhập từ bàn phím và ghi thay đổi vào file.") 
+    def tim_nhan_vien_theo_khoang_luong():
+        print("Y6. Tìm các nhân viên theo khoảng lương nhập từ bàn phím.")
+    def sap_xep_theo_ho_ten():
+        print("Y7. Sắp xếp nhân viên theo họ và tên.")
+    def sap_xep_theo_thu_nhap():
+        print("Y8. Sắp xếp nhân viên theo thu nhập.")
+    def xuat_5_nhan_vien_thu_nhap_cao_nhat():
+        print("Y9. Xuất 5 nhân viên có thu nhập cao nhất.")
